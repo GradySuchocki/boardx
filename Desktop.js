ab240cb6e130d85ff24f765d3b09a537be7ee4d1
GradySuchocki <157739832+GradySuchocki@users.noreply.github.com>
2024-04-06 17:38:38 -0400

    Create Desktop.js

---
+++ Desktop.js
@@ -0,0 +1,12 @@
+import FrameComponent from "../components/FrameComponent";
+import styles from "./Desktop.module.css";
+
+const Desktop = () => {
+  return (
+    <div className={styles.desktop2}>
+      <FrameComponent />
+    </div>
+  );
+};
+
+export default Desktop;
